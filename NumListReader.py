93453125ec909691369787d5f55c8b04b646795f
youja@Surface-Pro <iamjasonlin@163.com>
2018-09-01 18:07:23 +0800

    commited NumListReader

---
+++ NumListReader.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def ReadNumList(string):
+    List = []
+    s = ''
+    for i in range(len(string)):
+        if (string[i] == ' '):
+            List.append(int(s))
+            s = ''
+        elif (i == len(string)-1):
+            List.append(int(s+string[i]))
+        else:
+            s += string[i]
+    return List
+
+if __name__ == "__main__":
+    print(ReadNumList(input()))
+    input()
